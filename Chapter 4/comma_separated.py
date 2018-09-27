762c4013190b27ec34f24b33f68ac7568dabf389
PushkarGarg02 <43633663+PushkarGarg02@users.noreply.github.com>
2018-09-27 13:54:07 +0530

    Update comma_separated.py

--- Chapter 4/comma_separated.py
+++ Chapter 4/comma_separated.py
@@ -1,1 +1,15 @@
+spam = ['apples','bananas','tofu','cats']
+
+def functionTemp(parameterList):
+    str = ''
+    
+    for itemIndex in range(len(parameterList)):
+        if itemIndex != (len(parameterList)-1):
+            str = str + parameterList[itemIndex] + ', '
+        else:
+            str = str + 'and ' + parameterList[itemIndex] + '.'
+    
+    return str
+
+print(functionTemp(spam))
 
