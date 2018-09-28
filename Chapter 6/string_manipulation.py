5d9aa0d01e2d8e46df349d3a52d858a2585eaa57
PushkarGarg02 <43633663+PushkarGarg02@users.noreply.github.com>
2018-09-28 14:20:04 +0530

    Create string_manipulation.py

---
+++ Chapter 6/string_manipulation.py
@@ -0,0 +1,30 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+
+def printTable(tableData):
+    tableLengthList = []
+    
+    for mainList in tableData:
+        maximumLengthOfItem = 0
+        
+        for subItem in mainList[len(mainList)]:
+            lengthOfItem = len(subItem)
+            if lengthOfItem > maximumLengthOfItem:
+                maximumLengthOfItem = lengthOfItem
+        
+        tableLengthList.append(maximumLengthOfItem)
+        
+    
+    for i in range(len(tableData[0])):
+        for j in range(len(tableLengthList)):
+            print(tableData[j][i].rjust(tableLengthList[j]))
+            
+        
+printTable(tableData)
+
+#  apples Alice  dogs
+# oranges   Bob  cats
+#cherries Carol moose
+#  banana David goose
