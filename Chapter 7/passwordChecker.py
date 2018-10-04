ee23e44f0000aab01f4774f0d5f7b73022118df4
PushkarGarg02 <43633663+PushkarGarg02@users.noreply.github.com>
2018-10-04 11:26:43 +0530

    Update passwordChecker.py

--- Chapter 7/passwordChecker.py
+++ Chapter 7/passwordChecker.py
@@ -2,17 +2,20 @@ import re
 
 inputPasswordforValidation = str(input())
 
-lowerCaseRegex = re.compile(r'[a-z]+')
-upperCaseRegex = re.compile(r'[A-Z]+')
-digitRegex = re.compile(r'\d+')
+def passwordValidator(inputPasswordforValidation):
+    lowerCaseRegex = re.compile(r'[a-z]+')
+    upperCaseRegex = re.compile(r'[A-Z]+')
+    digitRegex = re.compile(r'\d+')
 
-if len(inputPasswordforValidation) <8:
-    print('Password must be at least 8 characters.')
-elif lowerCaseRegex.search(inputPasswordforValidation) == None:
-    print('Password must contain at least one lower case letter.')
-elif upperCaseRegex.search(inputPasswordforValidation) == None:
-    print('Password must contain at least one upper case letter.')
-elif digitRegex.search(inputPasswordforValidation) == None:
-    print('Password must contain at least one digit.')
-else:
-    print('Wonderful! Your password is strong.')
+    if len(inputPasswordforValidation) <8:
+        print('Password must be at least 8 characters.')
+    elif lowerCaseRegex.search(inputPasswordforValidation) == None:
+        print('Password must contain at least one lower case letter.')
+    elif upperCaseRegex.search(inputPasswordforValidation) == None:
+        print('Password must contain at least one upper case letter.')
+    elif digitRegex.search(inputPasswordforValidation) == None:
+        print('Password must contain at least one digit.')
+    else:
+        print('Wonderful! Your password is strong.')
+        
+passwordValidator(inputPasswordforValidation)
