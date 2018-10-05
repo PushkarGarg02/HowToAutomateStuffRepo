ba4b87cca2cc13bf2a139837f2aec31586fa1233
Pushkar Garg <43633663+PushkarGarg02@users.noreply.github.com>
2018-10-05 20:42:15 +0530

    Update searchFileData.py

--- Chapter 8/searchFileData.py
+++ Chapter 8/searchFileData.py
@@ -7,18 +7,19 @@ fileHandle = open('test2.txt','w')
 fileHandle.write('This is the sample file and this would be second file which was created.')
 fileHandle.close()
 
-fileHandle = open('test3.text','w')
+fileHandle = open('test3.txt','w')
 fileHandle.write('Hello Friend, We are please to inform you that this would be the third file in this test code.')
 fileHandle.close()
 
-import os
-print(' '.join(os.listdir()))
-
 #Main code to search for a regular expression in all the .txt file is starting here
-import re
+import os, re
 
-#inputRegularExpression = str(input('Enter your regular expresion to search for string:'))
+inputRegularExpression = str(input('Enter your regular expresion to search for string:'))
+inputRegularExpressionRegex = re.compile(inputRegularExpression)
 
-fileFormatRegex = re.compile('[a-zA-Z0-9]+\.txt$')
+fileFormatRegex = re.compile('[a-zA-Z0-9]+\.txt')
 for files in fileFormatRegex.findall(' '.join(os.listdir())):
-   print(files)
+    fileHandler = open(files,'r')
+    for line in fileHandler.readlines():
+        if inputRegularExpressionRegex.search(line) != None:
+            print('File %s: %s' %(files,line))
