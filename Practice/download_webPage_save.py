e0ff39bb34697faa11d1ee7b69d49d4972d0ea42
Pushkar Garg <43633663+PushkarGarg02@users.noreply.github.com>
2018-10-16 16:44:59 +0530

    Create download_webPage_save.py

---
+++ Practice/download_webPage_save.py
@@ -0,0 +1,30 @@
+import os, requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.xkcd.com'
+os.makedirs('xkcd', exist_ok=True)
+
+while not url.endswith('#'):
+    print('Downloading the webpage...%s' % url)
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text)
+
+comicElem = soup.select('#comic img')
+if comicElem == []:
+    print('Could not find comic image')
+else:
+    comicUrl = comicElem[0].get('src')
+    print('Downloading image...%s' % (comicUrl))
+    res = requests.get(comicUrl)
+    res.raise_for_status()
+
+imageFile = open(os.path.join('xkcd',os.path.basename(comicUrl),'wb'))
+for chunk in res.iter_content(100000):
+    imageFile.write(chunk)
+imageFile.close()
+
+prevLink = soup.select('a[rel="prev"]')[0]
+url = 'http://xkcd.com' + prevLink.get('href')
+
+print('Done')
