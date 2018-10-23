bef88f9888165385210cb2747dfae84d33374542
Pushkar Garg <43633663+PushkarGarg02@users.noreply.github.com>
2018-10-23 12:11:59 +0530

    Create multiplicationTable.py

---
+++ Chapter 10/multiplicationTable.py
@@ -0,0 +1,43 @@
+#! python3
+
+import openpyxl, sys
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import NamedStyle, Font
+
+#Create a workbook
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+#Make named style for bold data
+makingBold = NamedStyle(name='makingBold')
+makingBold.font = Font(bold=True)
+
+#Making entry for rows
+row = 1
+for column in range(1,8):
+	columnLetter = get_column_letter(column)
+	if columnLetter+str(row) == 'A1':
+		continue
+	else:
+		sheet[columnLetter+str(row)] = column-1
+		sheet[columnLetter+str(row)].style = makingBold
+
+#Making entry for columns
+column=1
+columnLetter = get_column_letter(column)
+for row in range(1,8):
+	if columnLetter+str(row) == 'A1':
+		continue
+	else:
+		sheet[columnLetter+str(row)] = row-1
+		sheet[columnLetter+str(row)].style = makingBold
+		
+#Making rest of the entry		
+rowLetter = get_column_letter(1)
+for row in range(2,8):
+	for column in range(2,8):
+		columnLetter = get_column_letter(column)
+		sheet[columnLetter+str(row)] = sheet[columnLetter+str(1)].value * sheet[rowLetter+str(row)].value
+
+#Save the file
+wb.save('D:\\Scripts\\Multiplication_Table.xlsx')
