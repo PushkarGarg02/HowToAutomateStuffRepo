fe49ce6753eafb87a6e4f55d0cf3062dcd8fe15a
Pushkar Garg <43633663+PushkarGarg02@users.noreply.github.com>
2018-10-29 18:01:49 +0530

    Update fetchWeatherData.py

--- Practice/Chapter 14/fetchWeatherData.py
+++ Practice/Chapter 14/fetchWeatherData.py
@@ -3,19 +3,25 @@
 import requests, json, sys
 
 if len(sys.argv) > 3:
+	print(len(sys.argv), sys.argv[1],sys.argv[2],sys.argv[3])
 	print('Usage: fetchWeatherData.py location')
 	sys.exit()
 
 location = ','.join(sys.argv[1:])
+appkey = '844b6ee70e130299f79282d04f3b0d9f'
 
-url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % (location)
+url = 'http://api.openweathermap.org/data/2.5/forecast?q=%s&cnt=3&appid=%s' % (location,appkey)
 response = requests.get(url)
 response.raise_for_status()
 weatherData=json.loads(response.text)
-print(weatherData)
-
-
-'''
-http://api.openweathermap.org/data/2.5/forecast/daily?q=Sanfrancisco,CA&cnt=3
-'''
+w = weatherData['list']
+print('Current weather in %s:' % (location))
+print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+print()
+print('Tomorrow:')
+print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+print()
+print('Day after Tomorrow:')
+print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
+print()
 
